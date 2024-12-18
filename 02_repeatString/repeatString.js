c29dba7895aa08277f59a7cef1bc9a74259ba57a
Deven Bhattacharya <devenbhattacharya@gmail.com>
2024-12-18 11:18:14 -0500

    completed exercise to repeat strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+function repeatString(str, num) {
+    if (num < 0) {
+        return 'ERROR'; // Handle negative numbers
+    }
 
-};
+    let result = ''; // Initialize an empty string to store the result
+
+    for (let i = 0; i < num; i++) {
+        result += str; // Append the string `num` times
+    }
+
+    return result; // Return the final repeated string
+}
 
 // Do not edit below this line
 module.exports = repeatString;
