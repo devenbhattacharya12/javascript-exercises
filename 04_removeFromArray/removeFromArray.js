3c1f2eeb5adf9f09021266b7657f4edc17058b23
Deven Bhattacharya <devenbhattacharya@gmail.com>
2024-12-18 16:22:18 -0500

    completed remove from array exercise using array ... values to remove element => valuesTo remove features

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...valuesToRemove) {
+    return array.filter (element => !valuesToRemove.includes(element));
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
