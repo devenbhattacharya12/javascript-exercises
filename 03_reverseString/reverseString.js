9a183a8ecf799c79d0b1a16647660b47eb68b9f3
Deven Bhattacharya <devenbhattacharya@gmail.com>
2024-12-18 15:09:09 -0500

    reverse string completed using for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversed = ""; // initialize the empty string
+    for (let i=str.length-1; i>=0; i--) {
+        reversed += str [i]; //append each character in reverse order
+    }
+return reversed
 };
+
+reverseString ('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
