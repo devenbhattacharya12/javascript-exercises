7506c631cbd4bce49717258ae4c80f496b769219
Deven Bhattacharya <devenbhattacharya@gmail.com>
2024-12-19 09:01:58 -0500

    check if a year is a leap year using if else

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(yearA) {
+    // A leap year is divisible by 4 but not by 100 unless also divisible by 400
+    if (yearA % 4 === 0 && (yearA % 100 !== 0 || yearA % 400 === 0)) {
+        return true; // It's a leap year
+    } else {
+        return false; // It's not a leap year
+    }
 };
 
 // Do not edit below this line
